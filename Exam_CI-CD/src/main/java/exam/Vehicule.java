8392642f32fb9f489132d9d48392774abbce205e
Paolo-Roucho <p.rouchaud@betclicgroup.com>
2022-11-28 14:15:27 +0100

    Creation des 5 classes

---
+++ Exam_CI-CD/src/main/java/exam/Vehicule.java
@@ -0,0 +1,45 @@
+package exam;
+
+public abstract class Vehicule {
+    Personne proprietaire;
+    int nbRoue;
+    int nbLitreMax;
+    int quantiteEssence;
+    TypeEssence typeEssence;
+    String modele;
+
+    public Vehicule(Personne proprietaire, int nbRoue, String modele, TypeEssence typeEssence, int nbLitreMax, int quantiteEssence) {
+        this.proprietaire = proprietaire;
+        this.modele = modele;
+        this.nbRoue = nbRoue;
+        this.typeEssence = typeEssence;
+        this.nbLitreMax = nbLitreMax;
+        this.quantiteEssence = quantiteEssence;
+    }
+
+    public void remplirEssence() {
+        quantiteEssence = nbLitreMax;
+
+        proprietaire.facture = nbLitreMax * typeEssence.prix;
+    }
+
+    public void remplirEssence(int quantity) {
+        if(quantiteEssence + quantity > nbLitreMax) {
+            quantiteEssence = nbLitreMax;
+        } else {
+            this.quantiteEssence += quantity;
+        }
+
+        proprietaire.facture = quantity * typeEssence.prix;
+    }
+    public boolean rouler() {
+        if(quantiteEssence > 0 && proprietaire.facture == 0) {
+            System.out.println("La voiture elle peut rouler là");
+            return true;
+        } else {
+            System.out.println("La voiture elle ne peut pas rouler là, faut faire quelque chose là");
+            return false;
+        }
+    }
+
+}
