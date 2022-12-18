255e2f5612fec928b5212f9c5fdb73db1062c68f
Paolo-Roucho <p.rouchaud@betclicgroup.com>
2022-12-18 14:19:47 +0100

    update

---
+++ Exam_CI-CD/.gitHooks/pre_push.java
@@ -0,0 +1,10 @@
+import java.nio.file.*;
+
+public class PrePush {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Vous avez effectué le push avec succès");
+        
+        System.exit(0);
+    }
+}
